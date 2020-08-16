ee6d9cd00af5ac9b4e58dbf9a32e232aa935bfde
Hzaakk <43729496+Hzaakk@users.noreply.github.com>
2020-08-16 16:08:22 +0300

    Update presetup.sh

--- presetup.sh
+++ presetup.sh
@@ -1,4 +1,5 @@
 sudo apt update
 sudo apt -y install ansible
-curl 'https://raw.githubusercontent.com/hzaakk/ansiblesetupplaybook/master/inventory' | sudo tee /etc/ansible/hosts
+curl 'https://raw.githubusercontent.com/hzaakk/ansiblesetupplaybook/master/ansible.cfg' > ~/.ansible.cfg
+curl 'https://raw.githubusercontent.com/hzaakk/ansiblesetupplaybook/master/inventory' > ~/.inventory
 curl 'https://raw.githubusercontent.com/hzaakk/ansiblesetupplaybook/master/playbook.yml' > ~/playbook.yml
