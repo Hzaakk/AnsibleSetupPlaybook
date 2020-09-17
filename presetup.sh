211d82eeaf0e20abf8d52ca1fe8d51285669130a
Hzaakk <43729496+Hzaakk@users.noreply.github.com>
2020-09-17 07:53:24 +0300

    Update presetup.sh

--- presetup.sh
+++ presetup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-if which apt > /dev/null; then
-        sudo apt update
-        sudo apt -y install ansible
-elif which yum > /dev/null; then
-        sudo yum check-update
-        sudo yum -y install ansible
+if ! [ "$1" -eq 0 ]; then
+        if which apt > /dev/null; then
+                sudo apt update
+                sudo apt -y install ansible
+        elif which yum > /dev/null; then
+                sudo yum check-update
+                sudo yum -y install ansible
+        fi
 fi
-
 master='https://raw.githubusercontent.com/hzaakk/ansiblesetupplaybook/master'
 curl  "${master}/ansible.cfg" > ~/.ansible.cfg
 curl "${master}/inventory" > ~/.inventory
